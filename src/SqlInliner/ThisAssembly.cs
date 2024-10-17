7abdabd10e205f84045f373885c83149e452ba6e
Steve Hansen <steve@hansenconsultancy.be>
2024-10-17 14:51:04 +0200

    chore(deps): Updated packages

--- src/SqlInliner/ThisAssembly.cs
+++ src/SqlInliner/ThisAssembly.cs
@@ -1,5 +1,9 @@
 ﻿// ReSharper disable once CheckNamespace
 partial class ThisAssembly
 {
+#if DEBUG
+    public const string AppName = Info.Product + " v" + Info.InformationalVersion + " - DEV";
+#else
     public const string AppName = Info.Product + " v" + Info.InformationalVersion + " - " + Metadata.RepositoryUrl;
+#endif
 }
